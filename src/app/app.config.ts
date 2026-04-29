db0cc2874c5a34bfd8d786bbc0e5663a2e236c09
menchaca-andres <4mrvro@gmail.com>
2026-04-29 02:05:25 -0400

    agregando reactiveforms

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { ApplicationConfig } from '@angular/core'
 import { provideRouter } from '@angular/router'
 import { provideHttpClient, withInterceptors } from '@angular/common/http'
+import { provideAnimations } from '@angular/platform-browser/animations'
 import { routes } from './app.routes'
 import { tokenInterceptor } from './core/interceptors/token.interceptor'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([tokenInterceptor]))
+    provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideAnimations()
   ]
 }
