4174b6bb2dd05169c6977048bc218aa3bb1fe262
menchaca-andres <4mrvro@gmail.com>
2026-04-29 01:57:04 -0400

    cargando patch

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { AuthStore } from './core/store/auth.store'
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  template: `<router-outlet />`
 })
-export class App {
-  protected readonly title = signal('frontend-siceref');
+export class App implements OnInit {
+  private authStore = inject(AuthStore)
+
+  ngOnInit(): void {
+    this.authStore.cargarDesdeToken()
+  }
 }
