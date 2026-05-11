1eb1c241a3a7e7688e44fa9d39583fe8a8d7c3f1
menchaca-andres <4mrvro@gmail.com>
2026-05-11 17:26:02 -0400

    implementas nuevos permisos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -48,6 +48,16 @@ export const routes: Routes = [
                         path: 'roles',
                         canActivate: [permissionGuard('roles:obtener')],
                         loadComponent: () => import('./features/superadmin/roles/roles').then(m => m.RolesComponent)
+                    },
+                    {
+                        path: 'admins-sistema',
+                        canActivate: [permissionGuard('admins-sistema:crear')],
+                        loadComponent: () => import('./features/superadmin/admins-sistema/admins-sistema').then(m => m.AdminsSistemaComponent)
+                    },
+                    {
+                        path: 'admins-refugio',
+                        canActivate: [permissionGuard('admins-refugio:crear')],
+                        loadComponent: () => import('./features/superadmin/admins-refugio/admins-refugio').then(m => m.AdminsRefugioComponent)
                     }
                 ]
             },
@@ -68,6 +78,16 @@ export const routes: Routes = [
                         path: 'especies',
                         canActivate: [permissionGuard('especies:obtener')],
                         loadComponent: () => import('./features/refugio/especies/especies').then(m => m.EspeciesComponent)
+                    },
+                    {
+                        path: 'trabajadores',
+                        canActivate: [permissionGuard('trabajadores:obtener')],
+                        loadComponent: () => import('./features/refugio/trabajadores/trabajadores').then(m => m.TrabajadoresComponent)
+                    },
+                    {
+                        path: 'mi-refugio',
+                        canActivate: [permissionGuard('refugio:obtener:propio')],
+                        loadComponent: () => import('./features/refugio/mi-refugio/mi-refugio').then(m => m.MiRefugioComponent)
                     }
                 ]
             },
