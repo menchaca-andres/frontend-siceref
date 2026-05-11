c81cf45502cccbaa16c7401cdeed9df43d55486c
menchaca-andres <4mrvro@gmail.com>
2026-05-11 15:55:54 -0400

    registerResponse usa usuario para usuario o worker

--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { environment } from '../../../../environment/environment'
-import { LoginDto, RegisterDto, RegisterWorkerDto, AuthResponse } from '../../../core/models/auth/auth.model'
+import { LoginDto, RegisterDto, RegisterWorkerDto, AuthResponse, RegisterResponse } from '../../../core/models/auth/auth.model'
 
 @Injectable({
     providedIn: 'root'
@@ -15,15 +15,19 @@ export class AuthService {
         return this.http.post<AuthResponse>(`${this.apiUrl}/auth/login`, data)
     }
 
-    register(data: RegisterDto): Observable<any> {
-        return this.http.post(`${this.apiUrl}/auth/register`, data)
+    register(data: RegisterDto): Observable<RegisterResponse> {
+        return this.http.post<RegisterResponse>(`${this.apiUrl}/auth/register`, data)
     }
 
-    registerWorker(data: RegisterWorkerDto): Observable<any> {
-        return this.http.post(`${this.apiUrl}/auth/register/worker`, data)
+    registerWorker(data: RegisterWorkerDto): Observable<RegisterResponse> {
+        return this.http.post<RegisterResponse>(`${this.apiUrl}/auth/register/worker`, data)
     }
 
-    registerSuperadmin(data: RegisterDto): Observable<any> {
-        return this.http.post(`${this.apiUrl}/auth/register/superadmin`, data)
+    registerSuperadmin(data: RegisterDto): Observable<RegisterResponse> {
+        return this.http.post<RegisterResponse>(`${this.apiUrl}/auth/register/superadmin`, data)
+    }
+
+    registerAdminRefugio(data: RegisterWorkerDto): Observable<RegisterResponse> {
+        return this.http.post<RegisterResponse>(`${this.apiUrl}/auth/register/admin-refugio`, data)
     }
 }
