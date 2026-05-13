6691cc896edf74c3183fe298e88b691f7d38c15e
menchaca-andres <4mrvro@gmail.com>
2026-05-13 02:20:53 -0400

    rediseno con boxicons y minimizacion. dependiendo los permisos del ususario se mostraran las pestanas

--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,12 +1,19 @@
-import { Component, inject } from '@angular/core'
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core'
 import { RouterLink, RouterLinkActive } from '@angular/router'
 import { AuthStore } from '../../../core/store/auth.store'
 
 @Component({
   selector: 'app-sidebar',
   imports: [RouterLink, RouterLinkActive],
-  templateUrl: './sidebar.html'
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.scss'
 })
 export class SidebarComponent {
   authStore = inject(AuthStore)
+  @Input() collapsed = false
+  @Output() collapsedChange = new EventEmitter<boolean>()
+
+  toggleCollapsed(): void {
+    this.collapsedChange.emit(!this.collapsed)
+  }
 }
