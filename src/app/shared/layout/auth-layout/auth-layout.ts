15216ab2d450864cb65c6621c7e5cca2edce15f1
menchaca-andres <4mrvro@gmail.com>
2026-05-27 11:38:44 -0400

    importando scss y definiendo background

--- src/app/shared/layout/auth-layout/auth-layout.ts
+++ src/app/shared/layout/auth-layout/auth-layout.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router'
 @Component({
   selector: 'app-auth-layout',
   imports: [RouterOutlet],
-  templateUrl: './auth-layout.html'
+  templateUrl: './auth-layout.html',
+  styleUrl: './auth-layout.scss'
 })
 export class AuthLayoutComponent { }
